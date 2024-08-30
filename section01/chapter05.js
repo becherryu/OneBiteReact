763580788ba12f4ebfbf90364e96e008a22f9cd1
becherryu <81617058+becherryu@users.noreply.github.com>
2024-08-30 21:10:43 +0900

    자료형

---
+++ section01/chapter05.js
@@ -0,0 +1,34 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+// console.log(num1 - num2);
+// console.log(num2 + num3);
+// console.log(num1 * num3);
+// console.log(num2 % num3);
+let inf = Infinity;
+let minf = -Infinity;
+
+let nan = NaN; // 수치연산 실패시 나옴
+//console.log(1 * "hello");
+
+// 2. String Type
+let myName = "눈송이";
+let myLocation = "청파";
+let introduce = myName + myLocation;
+//console.log(introduce);
+
+// 템플릿 리터럴 문법
+let introduceText = `${myName}은 ${myLocation}에 거주합니다.`; // 백틱은 변수의 값을 동적으로 적용 가능
+//console.log(introduceText);
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다))
+let empty = null;
+
+// 5. Undefined Type
+let none;
+//console.log(none);
