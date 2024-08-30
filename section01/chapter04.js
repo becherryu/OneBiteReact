f47d71aaf4516d926860a64b68a85864add660c2
becherryu <81617058+becherryu@users.noreply.github.com>
2024-08-30 20:58:24 +0900

    변수와 상수

---
+++ section01/chapter04.js
@@ -0,0 +1,23 @@
+// 1. 변수
+let age;
+//console.log(age);
+
+age = 30;
+//console.log(age);
+
+// 2. 상수 <- 초기값 할당 필수
+const birth = "0000.01.01";
+
+// 3. 변수 명명 규칙
+// 3-1. $나 _를 제외한 기호를 사용할 수 없다.
+let $_name;
+
+// 3-2. 숫자로 시작할 수 없다.
+let name1;
+
+// 3-3. 예약어를 사용할 수 없다.
+
+// 4. 변수 명명 가이드 (명확하게)
+let salesCount = 1;
+let refundCount = 1;
+let totalSalesCount = salesCount - refundCount;
